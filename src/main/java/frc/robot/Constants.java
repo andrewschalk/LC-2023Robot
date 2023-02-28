93774ea5d1ecbf59f41ec63178392bdffdd04474
Andrew <andrew.schalk1@gmail.com>
2023-02-28 16:59:57 -0600

    formatting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,9 +3,9 @@ package frc.robot;
 public final class Constants {
     
     //CAN IDs
-    public static final int backLeftDriveMotorCAN = 1;
-    public static final int frontLeftDriveMotorCAN = 2;
-    public static final int backRightDriveMotorCAN = 3;
+    public static final int backLeftDriveMotorCAN   = 1;
+    public static final int frontLeftDriveMotorCAN  = 2;
+    public static final int backRightDriveMotorCAN  = 3;
     public static final int frontRightDriveMotorCAN = 4;
 
     //PWM channels
@@ -13,9 +13,9 @@ public final class Constants {
 
     //DIO channels
     public static final int armForwardLimitDIO = 0;
-    public static final int armBackLimitDIO = 1;
-    public static final int armEncoderADIO = 2;
-    public static final int armEncoderBDIO = 3;
+    public static final int armBackLimitDIO    = 1;
+    public static final int armEncoderADIO     = 2;
+    public static final int armEncoderBDIO     = 3;
 
     //Input device ports
     public static final int f310Port = 0;
